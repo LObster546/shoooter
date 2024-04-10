222fd86ead9a7a971b14651e53929e6cf53c5457
LObster546 <166622737+LObster546@users.noreply.github.com>
2024-04-10 17:18:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,3 +4,17 @@ from random import*
 
 #Главное меню
 #Здесь будет  меню с кнопкой "Играть", названием и управление
+app = QApplication([])
+main_win = QWidget()
+#кнопка
+pbutton = QPushButton('Играть')
+#шашлык
+vline = QVBoxLayout()
+
+vline.addWidget(pbutton)
+
+
+main_win.setLayout(vline)
+
+main_win.show()
+app.exec_()
