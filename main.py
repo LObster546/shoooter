42bc66a029c9a002794388ac1cfe001d9d91e35b
LObster546 <166622737+LObster546@users.noreply.github.com>
2024-04-10 17:05:57 +0300

    Update main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QVBoxLayout, QRadioButton, QMessageBox, QHBoxLayout, QGroupBox, QButtonGroup
+from random import*
+
+#Главное меню
+#Здесь будет  меню с кнопкой "Играть", названием и управление
